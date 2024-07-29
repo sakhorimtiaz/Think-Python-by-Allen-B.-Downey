2248fdebf77548dbc42600e03df0432c295cef7f
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2024-07-29 18:41:20 +0600

    Create 8-3.py

---
+++ Chapter-8/8-3.py
@@ -0,0 +1,13 @@
+#word = "bananahjgak"
+#print(word[::-1])
+
+"""def is_palindrome(word):
+    if word==word[::-1]:
+        return True
+    return False
+#print(is_palindrome("nayan"))"""
+
+#or
+def is_palindrome(word):
+    return word==word[::-1]
+print (is_palindrome("nayan"))
