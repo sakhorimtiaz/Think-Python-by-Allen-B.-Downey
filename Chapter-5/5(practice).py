e33ebf14af3facb4142d20bf8f99ccc243b872bc
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2024-07-30 00:02:21 +0600

    Create 5(practice).py

---
+++ Chapter-5/5(practice).py
@@ -0,0 +1,10 @@
+#practice
+def countdown(n):
+    if n<=0:
+        print("go")
+    else :
+        print(n)
+        countdown(n-1)
+#countdown(996)
+name=input("what is your name\n")
+print (name)
