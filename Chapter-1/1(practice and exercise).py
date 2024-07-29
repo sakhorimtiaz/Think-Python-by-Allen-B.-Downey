e4ee4b85998a0533ceb08db9898feff138b71654
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2024-07-29 23:20:47 +0600

    Create 1(practice and exercise).py

---
+++ Chapter-1/1(practice and exercise).py
@@ -0,0 +1,12 @@
+print ("hello?")
+p= 42*60+60
+q= 10/1.61
+9**2
+9^2 #bitwise operator XOR, output is in decimal 
+9<<2 #shift left in binary
+print (bin (11))
+print (bin (11)[2:]) # remving the prefix
+print (10,000,000)  #tuple
+10,000,000          #not executed
+print (10,111,000)
+10,111,000         #recent line
