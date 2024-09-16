2ece6d69d996a6665854c8566ece240a5ea46ea2
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2024-09-16 21:38:09 +0600

    Update 13-8.py

--- Chapter-13/13-8.py
+++ Chapter-13/13-8.py
@@ -1,12 +1,10 @@
-Question-1:
+#we will not rease punctuation, coz it will play a vital role in the code
 import random
-import string
 def make_word_dict():
-    with open("C:\\Users\\THINKPAD\\Downloads\\test(markov).txt") as file:
+    with open("C:\\Users\\THINKPAD\\Downloads\\emma.txt") as file:
         t=[]
         #d={}
         process=False
-        translation_table=str.maketrans("","",string.whitespace+string.punctuation)
         for line in file:
             if "*** START OF THE PROJECT" in line:
                 process=True
@@ -14,7 +12,7 @@ def make_word_dict():
             if process:
                 words=line.split()
                 for word in words:
-                    cleaned_word=word.translate(translation_table).lower()
+                    cleaned_word=word.lower()
                     if cleaned_word:
                         t.append(cleaned_word)
                         #d[cleaned_word]=d.get(cleaned_word,0)+1
@@ -27,8 +25,56 @@ def collection(h,prefix_length):
         prefix=tuple(h[i:i+prefix_length])
         suffix=h[i+prefix_length]
         if prefix not in prefix_suffix_map:
-            prefix_suffix_map[prefix]=[] #use set to ignore the repeated words in values, but list will be used for getting the random analysis
+            prefix_suffix_map[prefix]=[]
         prefix_suffix_map[prefix].append(suffix)
     return prefix_suffix_map
-print(h)
-print(collection(h,2))
+#print(h)
+def probability_list(c):
+    result_dict={}
+    for prefix,suffix in c.items():
+        total_words=len(suffix)
+        #create a new dictionary of mapping suffix words to relative frequencies
+        freq_map=count_suffix_frequencies(suffix)
+        #create a new dictionary of mapping suffix word to probabilty of getting a word
+        prob_map=probability_of_suffix(freq_map,total_words)
+        #modify the dictionary mapping prefix words (tuple) to probabilty of getting a word as suffix
+        result_dict[prefix]=prob_map
+    return result_dict
+def count_suffix_frequencies(p):
+    d = {}
+    for word in p:
+        d[word] = d.get(word, 0) + 1
+    return d
+
+def probability_of_suffix(f,total):
+    for key, value in f.items():
+        f[key] = value / total
+    return f
+#print(collection(h,4))
+
+#print(probability_list(c))
+
+def generate_random_text(prefix_suffix_map,prefix_length,max_words):
+    prefixes=list(prefix_suffix_map.keys())  #prepare a list of all keys
+    current_prefix=random.choice(prefixes)  #start with a random prefix, all are in tuple
+    generated_words=list(current_prefix)   # make a list to work with, as tuples are immutable
+
+    for i in range(max_words-prefix_length):
+        if current_prefix not in prefix_suffix_map:  #checking current_prefix, not the generated_word; as generated words are modified to list
+            break
+        suffixes=list(prefix_suffix_map[current_prefix].keys()) #making a list for the probable next word
+        weights=list(prefix_suffix_map[current_prefix].values())  #making a list for the weights next word
+        next_word=random.choices(suffixes,weights)[0]  #we will get a list from random.choices, so we need to call the 1st element
+
+        #we have a list of generated words
+        generated_words.append(next_word) # it updates the list
+
+        current_prefix=tuple(generated_words[-prefix_length:]) #current prefix is updated, must be in tuple as before
+    return " ".join(generated_words)
+
+
+max_words=50
+prefix_length=100
+c=collection(h,prefix_length)
+prefix_suffix_map=probability_list(c)
+print(generate_random_text(prefix_suffix_map,prefix_length,max_words))
