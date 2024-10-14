3cac1f9eb552205b4e3bfc2f27d4ad734839402d
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2024-10-14 23:40:36 +0600

    Add files via upload

---
+++ Chapter-14/14-1.py
@@ -0,0 +1,12 @@
+def sed(pattern_string,replacement_string,file1,file2):
+    try:
+        with open(file1, "r") as file1:
+            reading = file1.read()
+        replaced = reading.replace(pattern_string, replacement_string)
+        with open(file2, "w") as file2:
+            file2.write(replaced)
+    except:
+        print("something is wrong")
+
+sed("Emma","KT","C:\\Users\\THINKPAD\\Downloads\\emma.txt","text_write.txt")
+
